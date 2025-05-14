9430c2d8f2806470010480efadc553cf3148818e
Samuel Cañadas Ruiz <samuelcanadas2711@gmail.com>
2025-05-14 09:18:09 +0200

    Mostrar menú principal implementat

--- src/Vista/Vista.java
+++ src/Vista/Vista.java
@@ -4,4 +4,13 @@ public class Vista {
     public static void mostrarMissatge(String missatge) {
         System.out.println(missatge);
     }
+    
+    public static void mostrarMenuPrincipal() {
+        System.out.println("--------------------------------\n" +
+                           "------- BRAWL STARS VISA -------\n" +
+                           "--------------------------------\n" +
+                           "1. Llistar personatges\n" +
+                           "2. Administrar segons endpoint\n" +
+                           "3. Administrar segons JSON");
+    }
 }
