07d5eb13ab300bf8dc1d6022293177973b052e6f
Victor Extremera <vicres43@gmail.com>
2025-05-16 09:24:13 +0200

    Menus actualitzats al controlador

--- src/Controlador/MenuEndpoint.java
+++ src/Controlador/MenuEndpoint.java
@@ -1,6 +1,35 @@
 package Controlador;
 
+import java.util.Scanner;
+
 public class MenuEndpoint {
     public static void menuEndPoint() {
+        System.out.println("1. Mostrar llista de Brawlers amb ID segons l'endpoint");
+        System.out.println("2. Modificar personatge segons el l'endpoint amb ID");
+        System.out.println("3. Sortir");
+        System.out.print("Escolliu una opcio: ");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        switch (n) {
+            //***Modificar personatge segons l’endpoint***
+            //L’usuari escollirà quin personatge tractar mitjançant el ID que tenim a la BDD i executarem l’endpoint corresponent per aquest ID (https://api.brawlstars.com/v1/brawlers/[id]).
+            //Amb les dades resultants de l’endpoint preguntarem si vol actualitzar-lo o no.
+            //Disposarem d’un camp de tipus date que contindrà la data de la darrera actualització que s’ha fet.
+            case 1:
+                // Mostrar llista de cada nom i ID dels Brawlers a l'endpoint.
+
+                break;
+            case 2:
+                // Escollir l'ID per agafar les dades, mostrar-les i preguntar si les volem actualitzar.
+                // Afegir el cap data via BBDD amb CURDATE al UPDATE
+
+                break;
+            case 3:
+                // Sortir
+                System.out.println("Sortint...");
+                break;
+            default:
+                System.out.println("Opcio no valida");
+        }
     }
 }
--- src/Controlador/MenuJSON.java
+++ src/Controlador/MenuJSON.java
@@ -1,6 +1,31 @@
 package Controlador;
 
+import java.util.Scanner;
+
 public class MenuJSON {
     public static void menuJSON() {
+        System.out.println("1. Mostrar llista de Brawlers amb ID segons el JSON");
+        System.out.println("2. Modificar personatge segons l'endpoint amb ID");
+        System.out.println("3. Sortir");
+        System.out.print("Escolliu una opcio: ");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        switch (n) {
+            //***Modificar personatge segons el JSON***
+            //L’usuari escollirà quin personatge tractar mitjançant el ID que tenim a la BDD i cercarem el ID al JSON.
+            //Amb les dades resultants obtingudes del JSON, preguntarem si vol actualitzar-lo o no.
+            //Disposarem d’un camp de tipus date que contindrà la data de la darrera actualització que s’ha fet.
+            case 1:
+                // Llista dels Brawlers i el seu ID per saber quin buscar.
+
+                break;
+            case 2:
+                // Funcio per buscar aquest personatge al JSON i després preguntar si es vol actualitzar
+                // Afegir el  cap data via BBDD amb CURDATE al UPDATE
+
+                break;
+            default:
+                System.out.println("Opcio no valida");
+        }
     }
 }
