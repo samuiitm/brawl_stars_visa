4eeab4374db165b59a1adf2992c0fb5ffdb2c7df
Samuel Cañadas Ruiz <samuelcanadas2711@gmail.com>
2025-05-16 10:58:08 +0200

    Ruta del fitxer JSON corregida

--- src/Controlador/JSON/JsonDownloader.java
+++ src/Controlador/JSON/JsonDownloader.java
@@ -1,32 +1,43 @@
 package Controlador.JSON;
 
-import java.io.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class JsonDownloader {
-    public static void descarregarJson(String endpoint, String nomFitxer) {
+    public static void descarregarJson(String endpoint, String filePath) {
         try {
             URL url = new URL(endpoint);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            StringBuilder resposta = new StringBuilder();
-            String linia;
-
-            while ((linia = in.readLine()) != null) {
-                resposta.append(linia);
+            Scanner scanner = new Scanner(conn.getInputStream());
+            StringBuilder jsonSenseFormat = new StringBuilder();
+            while (scanner.hasNext()) {
+                jsonSenseFormat.append(scanner.nextLine());
             }
+            scanner.close();
 
-            in.close();
-            conn.disconnect();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonElement jsonElement = JsonParser.parseString(jsonSenseFormat.toString());
+            String jsonFormatejat = gson.toJson(jsonElement);
 
-            try (FileWriter fitxer = new FileWriter(nomFitxer)) {
-                fitxer.write(resposta.toString());
-            }
+            File file = new File(filePath);
+            file.getParentFile().mkdirs();
 
-            System.out.println("JSON desat a: " + nomFitxer);
+            FileWriter writer = new FileWriter(file);
+            writer.write(jsonFormatejat);
+            writer.close();
+
+            System.out.println("JSON descarregat i guardat correctament a " + filePath);
 
         } catch (IOException e) {
             e.printStackTrace();
