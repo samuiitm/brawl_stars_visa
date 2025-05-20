b61ca4ea17f8a6f90075d6a69d6352adbeb09792
Victor Extremera <vicres43@gmail.com>
2025-05-20 11:23:09 +0200

    existenBrawlers implementado al menu

--- src/Controlador/ExistenBrawlers.java
+++ src/Controlador/ExistenBrawlers.java
@@ -11,7 +11,7 @@ public class ExistenBrawlers {
     static final String API_URL = "https://api.brawlstars.com/v1/brawlers";
     static final String TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjI1ZTMxMzAxLWZlNjAtNGZiMy1iNzM1LTU1Yjk3YjhlMjgyYSIsImlhdCI6MTc0NzY0NzYwNSwic3ViIjoiZGV2ZWxvcGVyL2QwYTE0NWI4LTZlOTUtMDBlMS05ZmYyLTFkZDM3OTU5ZTQ5MyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiODUuNTAuMTYwLjQzIl0sInR5cGUiOiJjbGllbnQifV19.8UkJRM2dtbKVhwPpk3gv2kgm0_j-UhAqqK5E5T4wGQT5BP8J5mIc9qarZUbasEJ7C5z41n1FGSpAtz_W1jooNA";
 
-    public static void main(String[] args) {
+    public static void existenBrawlers() {
         try (Scanner sc = new Scanner(System.in);
              Connection conn = Model.ConnexioBD.getConnexio()) {
 
